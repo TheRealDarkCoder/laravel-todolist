2c1c59eda6ad10d2a54bca2680f56e98b5ecd406
TheRealDarkCoder <zuhayr2003@gmail.com>
2021-07-02 22:03:47 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+One of many quick laravel projects which I'm using to share laravel knowledge with friends and teaching the basics. Feel free to fork.
+
+
+*** Not maintained/efficient, just for educational purposes ***
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <h1> Todo list</h1>
+        <ul>
+            <li><a href="">Learn Laravel</a></li>
+            <li><a href="">Take dinner</a></li>
+            <li><a href="">Install Arch</a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
